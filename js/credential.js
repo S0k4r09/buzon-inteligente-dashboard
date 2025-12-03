6dceeaccb4ba4fcb9804d9cb27bc415da0b37e08
S0k4r09 <145043956+S0k4r09@users.noreply.github.com>
2025-12-03 19:59:12 -0300

    Update credential.js

--- js/credential.js
+++ js/credential.js
@@ -1,17 +1,16 @@
 function createMqttClient(clientId) {
-    const brokerUrl = 'wss://11c98208298d41e6aa134fb9838e6cf0.s1.eu.hivemq.cloud:443/mqtt';
+    const brokerUrl = 'wss://11c98208298d41e6aa134fb9838e6cf0.s1.eu.hivemq.cloud/mqtt';
     const options = {
+        port: 443,
         clientId: 'DashboardWeb_' + Math.random().toString(16).substr(2, 8),
         clean: true,
-        connectTimeout: 10000, // Aumentamos el tiempo de espera a 10s
+        connectTimeout: 10000,
         reconnectPeriod: 1000,
         protocol: 'wss',
         username: 'buzon.app',
         password: 'Hola1234',
     };
-    console.log(`Conectando a ${brokerUrl} con el cliente ID: ${options.clientId}`);
+    console.log(`Conectando a ${brokerUrl} en el puerto ${options.port} con el cliente ID: ${options.clientId}`);
     const client = mqtt.connect(brokerUrl, options);
     return client;
 }
-
-
