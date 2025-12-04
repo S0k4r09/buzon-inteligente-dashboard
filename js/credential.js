a60a764d399cd3dbb084faeb0851130530fa42d8
S0k4r09 <145043956+S0k4r09@users.noreply.github.com>
2025-12-03 21:21:12 -0300

    Update credential.js

--- js/credential.js
+++ js/credential.js
@@ -1,9 +1,10 @@
 // credential.js o tu archivo de configuración
 
-host = "31ece...s1.eu.hivemq.cloud";
-port = 8884;
-path = "/mqtt";
-clientId = "web_" + parseInt(Math.random() * 100, 10); 
+var mqttHost = "31ece5296d8e40029e588378218e87d0.s1.eu.hivemq.cloud"; // Tu host actual
+var mqttPort = 8884; // ¡IMPORTANTE: 8884 para WebSockets Seguros!
+var mqttUseSSL = true; // Obligatorio true
+var mqttUser = "buzon.app";
+var mqttPass = "BuzonApp123!"; // La que configuraste en HiveMQ
 
 client.connect({
     onSuccess: onConnect,
@@ -12,3 +13,4 @@ client.connect({
     password: "BuzonApp123!",
     onFailure: onFailure
 });
+
