c39fb6b06f7b61da009535f507e33ab353f9d6f0
S0k4r09 <145043956+S0k4r09@users.noreply.github.com>
2025-12-03 21:24:22 -0300

    Update credential.js

--- js/credential.js
+++ js/credential.js
@@ -5,12 +5,3 @@ var mqttPort = 8884; // ¡IMPORTANTE: 8884 para WebSockets Seguros!
 var mqttUseSSL = true; // Obligatorio true
 var mqttUser = "buzon.app";
 var mqttPass = "BuzonApp123!"; // La que configuraste en HiveMQ
-
-client.connect({
-    onSuccess: onConnect,
-    useSSL: true,
-    userName: "buzon.app",
-    password: "BuzonApp123!",
-    onFailure: onFailure
-});
-
